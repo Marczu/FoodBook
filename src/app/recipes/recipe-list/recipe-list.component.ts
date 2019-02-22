cd581dfe8cc2b90add9c4cccfa17589e6a166c7c
Marczu <marczu09@gmail.com>
2019-02-22 09:59:37 +0100

    Add html for recipe list and recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Testowy przepis', 'To jest testowy przepis',
+      'https://www.kwestiasmaku.com/sites/kwestiasmaku.com/files/ciasto_na_pizze_05_0.jpg'),
+    new Recipe('Drugi przepis', 'Dalsze testowanie mojego ulubionego przepisu traalala cos tam jeszcze',
+      'https://s3-media2.fl.yelpcdn.com/bphoto/ejfH5dBk4pm_PI_TnLaxWQ/ls.jpg')
+  ];
 
   constructor() { }
 
